cb7f9da6acc663cd6d090ca282ef6d1654b9f43a
Barjuan Davis <barjuandavis@gmail.com>
2020-06-18 18:19:43 +0700

    Updates

---
+++ precache-manifest.48e6824d1fcfcecff52f72eed60aecf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf04b1e2e4de6f93add8339184ebb2c",
+    "url": "/honk-generator/index.html"
+  },
+  {
+    "revision": "dbc6641c89628700bedd",
+    "url": "/honk-generator/static/css/main.b3b44f6a.chunk.css"
+  },
+  {
+    "revision": "723fe9b13410b0038a07",
+    "url": "/honk-generator/static/js/2.ba7a562d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/honk-generator/static/js/2.ba7a562d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc6641c89628700bedd",
+    "url": "/honk-generator/static/js/main.d9b35416.chunk.js"
+  },
+  {
+    "revision": "c5f3fc37f5ccc8c99338",
+    "url": "/honk-generator/static/js/runtime-main.2de13ac7.js"
+  },
+  {
+    "revision": "dda10c96a249aa1282d507f618afdad3",
+    "url": "/honk-generator/static/media/geese.dda10c96.svg"
+  }
+]);
